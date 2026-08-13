03c2bb67e88b9e2fe012344dca6b3e11e22c0ff9
ikz01 <iksferrera@gmail.com>
2026-08-13 23:25:56 +0800

    added new effects

---
+++ script.js
@@ -0,0 +1,25 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("visible");
+
+                observer.unobserve(entry.target);
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.05
+    }
+);
+
+cards.forEach((card) => {
+    observer.observe(card);
+});
