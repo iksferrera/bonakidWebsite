705fbc883162b1cfa1da67d2fd4067b339c9a3b9
ikz01 <iksferrera@gmail.com>
2026-08-14 20:22:03 +0800

    added video modal

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ const observer = new IntersectionObserver(
 cards.forEach((card) => {
     observer.observe(card);
 });
+
+
+// Video Modal
+
+function openVideo() {
+
+    const modal = document.getElementById("videoModal");
+    const video = document.getElementById("youtubeVideo");
+
+    video.src = "https://www.youtube.com/embed/Q9Ove2Oqd0Q?si=ShLDWZSSw8T5fQpT";
+
+    modal.style.display = "flex";
+}
+
+
+function closeVideo() {
+
+    const modal = document.getElementById("videoModal");
+    const video = document.getElementById("youtubeVideo");
+
+    modal.style.display = "none";
+
+    // Stop the video
+    video.src = "";
+}
+
+// Close the modal when clicking outside of the video
+
+
+window.addEventListener("click", function(event) {
+
+    const modal = document.getElementById("videoModal");
+
+    if (event.target === modal) {
+        closeVideo();
+    }
+
+});
